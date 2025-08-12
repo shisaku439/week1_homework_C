cafe1dfc71e2eab7862005671d4d8719f44e1a82
shisaku439 <shisaku555@gmail.com>
2025-08-12 12:00:42 +0900

    応用問題

--- customer.py
+++ customer.py
@@ -13,17 +13,12 @@ class Customer:
     # 入場料計算関数
     def entry_fee(self):
         # シニア料金(65歳以上): 1200円
-        if self.age >= 75:
-            return 500
-        elif self.age >= 65:
+        if self.age >= 65:
             return 1200
         # おとな料金(20歳以上65歳未満): 1500円
         elif self.age >= 20:
             return 1500
-        # こども料金(4歳以上20歳未満): 1000円
-        elif self.age > 3:
-            return 1000
-        return 0
+        return 1000
 
     # CSV形式で出力関数
     def info_csv(self):
@@ -56,15 +51,3 @@ print(ieyasu.entry_fee())  # 1200 という値を出力
 print(ken.info_csv())  # "Ken Tanaka,15,1000" という値を出力
 print(tom.info_csv())  # "Tom Ford,57,1500" という値を出力
 print(ieyasu.info_csv())  # "Ieyasu Tokugawa,75,1200" という値を出力
-
-# C-5. 3歳以下の入場料金の無料化
-print(ken.entry_fee())  # 1000 という値を出力
-print(tom.entry_fee())  # 1500 という値を出力
-print(ieyasu.entry_fee())  # 1200 という値を出力
-print(michelle.entry_fee())  # 0 という値を出力
-
-# C-6. 75歳以上の料金区分の追加
-print(ken.entry_fee())  # 1000 という値を出力
-print(tom.entry_fee())  # 1500 という値を出力
-print(ieyasu.entry_fee())  # 500 という値を出力
-print(michelle.entry_fee())  # 0 という値を出力
